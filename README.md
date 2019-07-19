148f580555a8ee096c92d0f68dee59df0adb60ea
IrvingHuang <jamek8@gmail.com>
2019-07-19 10:54:09 +0800

    說明補充

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # imageFilter
+Transform camera image to black and white like printer scan.
+
+這是一個簡單的Demo，因為我其實對於這些濾鏡也很不了解，所以沒有辦法真的調整得非常好，請見諒。
+我這邊先用相機隨便拍一個文件。
+![imageWithoutBrown](imageFilter/2.jpg)
+
+可以看到說這張照片其實有一點昏暗，拍攝的狀況並不十分理想。 那麼我這邊套用了兩種濾鏡。 成果如下
+![Mod_2](imageFilter/Mod_2.jpg)
+
+其實轉完之後有些字有點糊掉了，但是要把字調整得非常清晰，也會視每張圖的情況來看，那以下就來大概說明一下我做了什麼事情。
+
+
--- imageFilter/ViewController.swift
+++ imageFilter/ViewController.swift
@@ -7,38 +7,59 @@
 //
 
 import UIKit
+import Metal
 import MetalKit
+
+let fileName = "2.jpg"
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var vImage: UIImageView!
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
         
         ///目前只能吃jpg的圖片，png之類的檔案可能會掛掉
-        let image = UIImage(named: "867873.jpg")!
+        let image = UIImage(named: fileName)!
         let imageResult = image.convertToBW()
         vImage.image = imageResult
+        imageResult?.saveToDisk()
     }
-
 }
 
 extension UIImage {
+    func saveToDisk() {
+        let manager = FileManager.default
+        let urlForDocument = manager.urls(for: .documentDirectory, in:.userDomainMask)
+        let url = urlForDocument[0] as URL
+        print(url.path)
+
+        let data = self.jpegData(compressionQuality: 1.0)
+
+        let path = url.path + "/Mod_" + fileName
+        let createSuccess = manager.createFile(atPath: path ,contents:data,attributes:nil)
+        print("檔案建立結果: \(createSuccess)")
+    }
+    
     func convertToBW() -> UIImage? {
         
         ///進行遮罩把不算太黑的顏色都蓋成透明
         let imageWithoutBrown = masklightToMidRangeBrown(inputImage: self)!
         
         ///進行對比等等的調整使其只剩下黑白兩色
-        let imageConvertBlackWhite = getScannedImage(inputImage: imageWithoutBrown)!
+        if let imageConvertBlackWhite = getScannedImage(inputImage: imageWithoutBrown) {
+            return imageConvertBlackWhite
+        }
 
-        return imageConvertBlackWhite
+        return self
     }
     
     private func masklightToMidRangeBrown(inputImage: UIImage) -> UIImage? {
-        let myColorMaskedImage = inputImage.cgImage?.copy(maskingColorComponents: [80, 254,  80, 254, 80, 254])!  //[124, 255,  68, 222, 0, 165]
+        let myColorMaskedImage = inputImage.cgImage?.copy(maskingColorComponents: [100, 180,  100, 180, 20, 180])!  //[124, 255,  68, 222, 0, 165]
         let context = CIContext()
         let temp = convertCGImageToCIImage(cgImage: myColorMaskedImage!)
         if let cgimg = context.createCGImage(temp, from: temp.extent) {
@@ -53,8 +74,16 @@ extension UIImage {
     
     private func getScannedImage(inputImage: UIImage) -> UIImage? {
         
-        let openGLContext = EAGLContext(api: .openGLES2)
-        let context = CIContext(eaglContext: openGLContext!)
+
+        let context: CIContext
+        
+        /// metal only can be used on REAL device
+        if let device = MTLCreateSystemDefaultDevice() {
+            context = CIContext(mtlDevice: device)
+        }
+        else {
+            context = CIContext()
+        }
         
         let filter = CIFilter(name: "CIColorControls")
         let coreImage = CIImage(image: inputImage)
@@ -63,7 +92,7 @@ extension UIImage {
         //Key value are changable according to your need.
 
         filter?.setValue(7, forKey: kCIInputContrastKey)
-        filter?.setValue(1, forKey: kCIInputSaturationKey)
+        filter?.setValue(0, forKey: kCIInputSaturationKey)
         filter?.setValue(1.2, forKey: kCIInputBrightnessKey)
         
         
